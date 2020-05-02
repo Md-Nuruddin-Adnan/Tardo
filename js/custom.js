53f6278d3b64554757c222def087127021034c75
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2020-05-02 19:09:14 +0600

    channel page added

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,9 @@
-// Add active class to the main nav
-var header = document.getElementById("main-nav")
-var btns = header.getElementsByClassName("nav-link");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    });
-}
-
 $('.toggle_btn').on('click', function(e) {
-    $('.toggle-area').toggleClass("d-none"); //you can list several class names 
-    // e.preventDefault();
-  });
+  $('.toggle-area').toggleClass("d-none"); //you can list several class names 
+  // e.preventDefault();
+});
+
+$('.emoji-btn').on('click', function(e) {
+  $('.emoji-box').toggleClass("d-none"); //you can list several class names 
+  // e.preventDefault();
+});
